732adf8314f5cd08e10d69998c6ca2de16a99f90
Mochida <2111140020j@ed.fuk.kindai.ac.jp>
2023-11-22 08:38:18 +0000

    a

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,58 +1,26 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-
-  # GET /users
   def index
     @users = User.all
   end
-
-  # GET /users/1
-  def show
-  end
-
-  # GET /users/new
+  
   def new
     @user = User.new
   end
-
-  # GET /users/1/edit
-  def edit
-  end
-
-  # POST /users
+  
   def create
-    @user = User.new(user_params)
-
+    @user = User.new(
+      uid: params[:user][:uid],
+      password: params[:user][:password],
+      password_confirmation: params[:user][:password_confirmation])
     if @user.save
-      redirect_to @user, notice: 'User was successfully created.'
+      redirect_to users_path
     else
-      render :new
+      render 'new'
     end
   end
-
-  # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      redirect_to @user, notice: 'User was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /users/1
+  
   def destroy
-    @user.destroy
-    redirect_to users_url, notice: 'User was successfully destroyed.'
+    User.find(params[:id]).destroy
+    redirect_to root_path
   end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:password, :password_confirmation)
-    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,9 @@
 
 ActiveRecord::Schema[7.1].define(version: 2023_11_15_082726) do
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "password"
+    t.string "uid"
+    t.string "pass"
+    t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
