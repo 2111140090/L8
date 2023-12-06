5bde4c958c9af13fe792d28b82594bcedf5e22e5
Hikaru Kameo <2111140090p@ed.fuk.kindai.ac.jp>
2023-12-06 08:57:02 +0000

    bbbbbbbbb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     has_many :tweets, dependent: :destroy #ユーザを消すとツイートも消える
     has_many :likes, dependent: :destroy #ユーザを消すといいねも消える
     has_many :like_tweets, source: :tweet, through: :likes
+    has_many :post_comments, dependent: :destroy
 
     validates :uid,  presence: true
     validates :uid, uniqueness: true
