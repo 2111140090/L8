ca267d5bd38807cca6f1a10c53fa0dbbe16bcf73
Kal'tsit15 <2111140020j@ed.fuk.kindai.ac.jp>
2023-12-13 07:52:07 +0000

    a

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,18 @@ Rails.application.routes.draw do
   get 'likes/destroy'
   post 'top/login'
   get 'top/login_form'
+  post 'top/login_form'
   get 'top/login'
   post 'top/login'
   get 'top/logout'
   post 'top/edit'
+  get 'users/new'
+  
   
   root 'videos#index'
   get 'videos/index'
+  get 'videos/new'
+  get 'videos/show'
   resources :users
   resources :videos
   resources :likes, only: [:create, :destroy]
