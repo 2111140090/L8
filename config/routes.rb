943b756c96a625003fec71fb81b77ad122e76b3b
Kal'tsit15 <2111140020j@ed.fuk.kindai.ac.jp>
2024-01-15 09:05:55 +0000

    a

--- config/routes.rb
+++ config/routes.rb
@@ -6,16 +6,23 @@ Rails.application.routes.draw do
   get 'top/login'
   post 'top/login'
   get 'top/logout'
+  get 'top/error'
   post 'top/edit'
   get 'users/new'
+  get 'users/index'
   post "likes/create"
   get "likes/destroy"
-#  get 'users/exist_error'
-#  get 'users/registered'
+  get 'users/exist_error'
+  get 'users/pw_error'
+  get 'users/registered'
+  get 'users/:uid' => 'users#destroy'
 
   
   root 'videos#index'
   get 'videos/index'
+  get 'videos/error'
+  get 'videos/nofile'
+  get 'videos/notitle'
   get "up" => "rails/health#show", as: :rails_health_check
   resources :users
   resources :videos
